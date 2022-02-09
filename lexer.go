6d81575d1e3bd7dd97fb16346cdfee04bc764804
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2022-02-09 03:46:54 -0800

    debug

--- lexer.go
+++ lexer.go
@@ -654,8 +654,9 @@ func (s *Scanner) scanComment(ch rune, t string) rune {
 						fmt.Println("Extensions:", v)
 						fmt.Println(curext[ext])
 						curlen := len(s.CommentStatus[v])
+						fmt.Println("Curlen:", curlen)
 						if Extensions[v].startSingle != "" {
-							fmt.Println("COMP:", string(ch), "&", string(Extensions[v].startSingle[curlen]))
+							fmt.Println("COMP:", string(ch), "&", string(Extensions[v].startSingle[curlen-1]))
 							if s.CommentStatus[v] != Extensions[v].startSingle { //If we already have a full match then skip it
 								if string(ch) == string(Extensions[v].startSingle[curlen]) { //If this character matches the current character in the extension then append it else clear it because characters are not consecutive
 									fmt.Println("setting true")
