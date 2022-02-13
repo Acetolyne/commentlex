5912647bdb23002a1eed7c8c3993de7d86209769
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2022-02-13 12:22:24 -0800

    detect multiline comment until end of comment

--- lexer.go
+++ lexer.go
@@ -665,18 +665,16 @@ func (s *Scanner) scanComment(ch rune) rune {
 							s.CommentStatusMulti[v] = ""
 						}
 					} else {
-						for ch != EOF {
-							if Extensions[v].endMulti != "" {
-								if len(s.CommentStatusMultiEnd[v]) < len(Extensions[v].endMulti) {
-									if string(ch) == string(Extensions[v].endMulti[len(s.CommentStatusMultiEnd[v])]) {
-										s.CommentStatusMultiEnd[v] += string(ch)
-									} else {
-										s.CommentStatusMultiEnd[v] = ""
-									}
+						if Extensions[v].endMulti != "" {
+							if len(s.CommentStatusMultiEnd[v]) < len(Extensions[v].endMulti) {
+								if string(ch) == string(Extensions[v].endMulti[len(s.CommentStatusMultiEnd[v])]) {
+									s.CommentStatusMultiEnd[v] += string(ch)
 								} else {
-									if Extensions[v].endMulti == s.CommentStatusMultiEnd[v] {
-										isMulti = true
-									}
+									s.CommentStatusMultiEnd[v] = ""
+								}
+							} else {
+								if Extensions[v].endMulti == s.CommentStatusMultiEnd[v] {
+									isMulti = true
 								}
 							}
 						}
