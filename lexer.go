da416b3a37c66717fcfbbace5a11cb82be94c352
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2022-02-13 21:38:16 -0800

    debug stuff

--- lexer.go
+++ lexer.go
@@ -660,7 +660,8 @@ func (s *Scanner) scanComment(ch rune) rune {
 					//fmt.Println(len(s.CommentStatusSingle[v]), len(Extensions[v].startSingle))
 					if len(s.CommentStatusSingle[v]) < len(SingleFull) {
 						if string(ch) != " " {
-							if string(ch) == string(SingleFull[len(s.CommentStatusSingle[v])+1]) {
+							fmt.Println("Comparing:", string(ch), SingleFull[len(s.CommentStatusSingle[v])])
+							if string(ch) == string(SingleFull[len(s.CommentStatusSingle[v])]) {
 								s.CommentStatusSingle[v] += string(ch)
 							} else {
 								s.CommentStatusSingle[v] = ""
