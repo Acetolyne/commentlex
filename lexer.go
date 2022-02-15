39b03cb14731d7e7d8c37f3a4af641d3aadcd874
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2022-02-15 15:12:47 -0800

    it doesnt work when you update the wrong variable

--- lexer.go
+++ lexer.go
@@ -709,7 +709,7 @@ func (s *Scanner) scanComment(ch rune) rune {
 							// 	//
 							fmt.Println("MultiEnded:", MultiEnded, len(s.CommentStatusMultiEnd[v]), len(Extensions[v].endMulti))
 							if len(s.CommentStatusMultiEnd[v]) < len(Extensions[v].endMulti) {
-								s.CommentStatusMultiAll[v] += string(ch)
+								s.CommentStatusMultiEnd[v] += string(ch)
 
 								// 	// 			if string(ch) == string(Extensions[v].endMulti[len(s.CommentStatusMultiEnd[v])]) {
 								// 	// 				s.CommentStatusMultiEnd[v] += string(ch)
