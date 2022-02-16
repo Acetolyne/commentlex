2eeebe8ada44700d48afb6bbf9411b9169c1d9df
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2022-02-15 23:10:27 -0800

    debug sMatch

--- lexer.go
+++ lexer.go
@@ -725,6 +725,7 @@ func (s *Scanner) scanComment(ch rune) rune {
 										return Comment
 									}
 								} else {
+									fmt.Println("sMatch is empty", s.Match)
 									return Comment
 								}
 							}
